233ce40827e55b42e2a26c20530963743325aa96
AndreyJavaEdu <scream123456@yandex.ru>
2024-06-27 22:28:54 +0500

    Настроили тестовый класс аннотациями.

---
+++ ProductMicroservice/src/test/java/com/kamenskiy/io/productmicroservice/ProductServiceIntegrationTest.java
@@ -0,0 +1,16 @@
+package com.kamenskiy.io.productmicroservice;
+
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+@DirtiesContext
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles(profiles = "test")
+@EmbeddedKafka(partitions = 3, count = 3, controlledShutdown = true)
+@SpringBootTest(properties = "spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}")
+public class ProductServiceIntegrationTest {
+
+}
